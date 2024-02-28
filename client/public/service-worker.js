d7acec71383052f3563fa98819876cdc2ad2f568
Isvi Lerma <isvi.lerma@gmail.com>
2024-02-28 12:18:22 -0700

    Código limpio

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,10 +1,8 @@
 // service-worker.js
-
 const CACHE_NAME = 'blog-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    // Agrega aquí otros archivos que deseas cachear
 ];
 
 self.addEventListener('install', (event) => {
@@ -20,20 +18,16 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
-                // Cache hit - return response
                 if (response) {
                     return response;
                 }
 
-                // No cache match - make network request and cache the response
                 return fetch(event.request)
                     .then((response) => {
-                        // Check if we received a valid response
                         if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
 
-                        // Clone the response as it's a stream that can only be consumed once
                         const responseToCache = response.clone();
 
                         caches.open(CACHE_NAME)
